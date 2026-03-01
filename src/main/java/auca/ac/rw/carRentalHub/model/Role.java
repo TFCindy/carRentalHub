a1c446b7d7e49590d714ac18159f3991323d1888
MUNEZERO-Cindy <cindyyymun55@gmail.com>
2026-03-01 19:49:22 +0200

    feat(model): add User and Role entities with Many-to-One relationship

--- src/main/java/auca/ac/rw/carRentalHub/model/Role.java
+++ src/main/java/auca/ac/rw/carRentalHub/model/Role.java
@@ -1,5 +1,27 @@
 package auca.ac.rw.carRentalHub.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
 public class Role {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(unique = true)
+	private String name;
+
+	public Role() {}
+
+	public Role(String name) { this.name = name; }
+
+	public UUID getId() { return id; }
+	public void setId(UUID id) { this.id = id; }
+
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
 }
--- src/main/java/auca/ac/rw/carRentalHub/model/User.java
+++ src/main/java/auca/ac/rw/carRentalHub/model/User.java
@@ -1,5 +1,53 @@
 package auca.ac.rw.carRentalHub.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(unique = true)
+	private String username;
+
+	@Column(unique = true)
+	private String email;
+
+	private String password;
+
+	// Simple role relationship: a user has one role
+	@ManyToOne
+	@JoinColumn(name = "role_id")
+	private Role role;
+
+	// User location (province/district/...)
+	@ManyToOne
+	@JoinColumn(name = "location_id")
+	private Location location;
+
+	public User() {}
+
+	// Getters and setters
+	public UUID getId() { return id; }
+	public void setId(UUID id) { this.id = id; }
+
+	public String getUsername() { return username; }
+	public void setUsername(String username) { this.username = username; }
+
+	public String getEmail() { return email; }
+	public void setEmail(String email) { this.email = email; }
+
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+
+	public Role getRole() { return role; }
+	public void setRole(Role role) { this.role = role; }
+
+	public Location getLocation() { return location; }
+	public void setLocation(Location location) { this.location = location; }
 }
